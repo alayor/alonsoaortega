a89438573ffc7168b576558359f75295b73a174e
alayor <alayor3@gmail.com>
2019-11-24 23:08:52 -0700

    add form and validate email

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,5 @@
 window.onload = function() {
   $(".page-item").click(function(e) {
-    debugger;
     const id = e.currentTarget.id.substr(-1);
     e.preventDefault();
     $(".skills-container").addClass("hide");
@@ -8,4 +7,24 @@ window.onload = function() {
     $(".page-item").removeClass("disabled");
     $(this).addClass("disabled");
   });
+  document.forms[0].onsubmit = validateForm;
 };
+
+function validateForm() {
+  var returnValue = true;
+
+  if (!validateEmail()) {
+    returnValue = false;
+  }
+  return returnValue;
+}
+
+function validateEmail() {
+  if (document.getElementById("inputEmail").value === "") {
+    $("#errorEmail").show();
+    return false;
+  } else {
+    $("#errorEmail").hide();
+    return true;
+  }
+}
